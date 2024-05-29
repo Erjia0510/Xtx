8864e30215096dc0c6d189db0eab0fc5160a2725
erjia0510 <2505841260@qq.com>
2024-05-29 21:38:00 +0800

    整体结构拆分以及分类实现（布局没问题）

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 导入对应包
+import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +17,14 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver({ importStyle: "sass" }),],
+      resolvers: [
+        //1.配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" })
+      ],
+    }),
+    // 按需定制主题配置
+    ElementPlus({
+      useSource: true,
     }),
   ],
   resolve: {
