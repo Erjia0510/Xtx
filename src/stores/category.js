d3ca00486e697daacb71c2316db9d109e2fdee74
erjia0510 <2505841260@qq.com>
2024-05-29 20:49:39 +0800

    pinia优化重复请求

---
+++ src/stores/category.js
@@ -0,0 +1,16 @@
+import { ref } from 'vue'
+import { getCategoryAPI } from '@/apis/layoutApi.js'
+import { defineStore } from 'pinia'
+
+export const useCategoryStore = defineStore('category', () => {
+  //导航列表的逻辑
+  const categoryList = ref([])
+  const getCategory = async () => {
+
+    const res = await getCategoryAPI()
+    console.log(res);
+    categoryList.value = res.data.result
+  }
+
+  return { categoryList, getCategory }
+})
