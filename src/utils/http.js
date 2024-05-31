4c0a1873811926fbc0345aa20f23e749f63611c4
erjia0510 <2505841260@qq.com>
2024-05-31 20:40:42 +0800

    表单基本功能实现

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -20,6 +22,12 @@ httpInstance.interceptors.response.use(function (response) {
   // 对响应数据做点什么
   return response;
 }, function (error) {
+  //统一错误提示
+  console.log(error)
+  ElMessage({
+    type: 'warning',
+    message: error.response.data.message
+  })
   // 超出 2xx 范围的状态码都会触发该函数。
   // 对响应错误做点什么
   return Promise.reject(error);
